183aae58555f85a267b50b6f1a547a2083c5feda
RadWolfie <RadWolfie@users.noreply.github.com>
2021-04-25 16:01:05 -0500

    Cleanly rebase chihiro-work on develop
    Co-authored-by: Luke Usher <luke.usher@outlook.com>
    Co-authored-by: wutno <aaron@installgentoo.net>
    Co-authored-by: RadWolfie <RadWolfie@users.noreply.github.com>

--- src/core/kernel/init/CxbxKrnl.cpp
+++ src/core/kernel/init/CxbxKrnl.cpp
@@ -329,6 +329,16 @@ void InitSoftwareInterrupts()
 }
 #endif
 
+void TriggerPendingConnectedInterrupts()
+{
+	for (int i = 0; i < MAX_BUS_INTERRUPT_LEVEL; i++) {
+		// If the interrupt is pending and connected, process it
+		if (HalSystemInterrupts[i].IsPending() && EmuInterruptList[i] && EmuInterruptList[i]->Connected) {
+			HalSystemInterrupts[i].Trigger(EmuInterruptList[i]);
+		}
+	}
+}
+
 static xbox::void_xt NTAPI CxbxKrnlInterruptThread(xbox::PVOID param)
 {
 	CxbxSetThreadName("CxbxKrnl Interrupts");
@@ -1341,7 +1351,7 @@ static void CxbxrKrnlInitHacks()
 	}
 	xbox::PsInitSystem();
 	xbox::KiInitSystem();
-	
+
 	// initialize graphics
 	EmuLogInit(LOG_LEVEL::DEBUG, "Initializing render window.");
 	CxbxInitWindow(true);
@@ -1468,7 +1478,7 @@ void CxbxrKrnlSuspendThreads()
 
 	// Don't use EmuKeGetPcr because that asserts kpcr
 	xbox::KPCR* Pcr = reinterpret_cast<xbox::PKPCR>(__readfsdword(TIB_ArbitraryDataSlot));
-	
+
 	// If there's nothing in list entry, skip this step.
 	if (!ThreadListEntry) {
 		return;
